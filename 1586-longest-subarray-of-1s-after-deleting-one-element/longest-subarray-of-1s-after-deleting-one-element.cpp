eac94f449b8fbe1c3cb609d0fbefd0f58908d3bf
ashu1osh9 <ashutosh.kr1818@gmail.com>
2026-05-21 23:10:08 +0530

    Time: 1 ms (33.75%) | Memory: 60 MB (97.38%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -1,97 +1,25 @@
-// class Solution {
-// public:
-
-//     int n;
-
-//     int findMax(vector<int>& nums, int skip_idx) {
-
-//         int current = 0;
-//         int maxLen = 0;
-
-//         for(int i = 0; i < n; i++) {
-
-//             if(i == skip_idx) {
-//                 continue;
-//             }
-
-//             if(nums[i] == 1) {
-//                 current++;
-//                 maxLen = max(maxLen, current);
-//             }
-//             else {
-//                 current = 0;
-//             }
-//         }
-
-//         return maxLen;
-//     }
-
-//     int longestSubarray(vector<int>& nums) {
-
-//         n = nums.size();
-
-//         int result = 0;
-//         bool hasZero = false;
-
-//         for(int i = 0; i < n; i++) {
-
-//             if(nums[i] == 0) {
-
-//                 hasZero = true;
-//                 result = max(result, findMax(nums, i));
-//             }
-//         }
-
-//         // all elements are 1
-//         if(!hasZero)
-//             return n - 1;
-
-//         return result;
-//     }
-// };
 class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
 
-        int n = nums.size();
-
-        vector<int> left(n, 0);
-        vector<int> right(n, 0);
-
-        // consecutive 1s from left
-        for(int i = 0; i < n; i++) {
-
-            if(nums[i] == 1) {
-
-                left[i] = 1;
-
-                if(i > 0)
-                    left[i] += left[i - 1];
-            }
-        }
-
-        // consecutive 1s from right
-        for(int i = n - 1; i >= 0; i--) {
-
-            if(nums[i] == 1) {
-
-                right[i] = 1;
-
-                if(i < n - 1)
-                    right[i] += right[i + 1];
-            }
-        }
-
+        int left = 0;
+        int zeroCount = 0;
         int ans = 0;
 
-        for(int i = 0; i < n; i++) {
+        for(int right = 0; right < nums.size(); right++) {
 
-            // delete current element
+            if(nums[right] == 0)
+                zeroCount++;
 
-            int leftOnes = (i > 0) ? left[i - 1] : 0;
-            int rightOnes = (i < n - 1) ? right[i + 1] : 0;
+            while(zeroCount > 1) {
 
-            ans = max(ans, leftOnes + rightOnes);
+                if(nums[left] == 0)
+                    zeroCount--;
+
+                left++;
+            }
+
+            ans = max(ans, right - left);
         }
 
         return ans;
