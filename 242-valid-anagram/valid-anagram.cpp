92bee82ce2d4e5ba524b91e6374d648f7026549f
ashu1osh9 <ashutosh.kr1818@gmail.com>
2026-05-24 14:10:28 +0530

    Time: 3 ms (45.43%) | Memory: 9.7 MB (48.29%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -2,25 +2,33 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
 
-        if(s.size() != t.size()) {
-            return false;
-        }
+        // if(s.size() != t.size()) {
+        //     return false;
+        // }
 
-        unordered_map<char,int> map;
+        // unordered_map<char,int> map;
 
-        for(int i = 0; i < s.size(); i++) {
-            map[s[i]]++;
-        }
+        // for(int i = 0; i < s.size(); i++) {
+        //     map[s[i]]++;
+        // }
 
-        for(int i = 0; i < t.size(); i++) {
+        // for(int i = 0; i < t.size(); i++) {
 
-            if(map[t[i]] <= 0) {
-                return false;
-            }
+        //     if(map[t[i]] <= 0) {
+        //         return false;
+        //     }
 
-            map[t[i]]--;
-        }
+        //     map[t[i]]--;
+        // }
 
+       // sorting 
+
+       sort(s.begin(),s.end());
+       sort(t.begin(),t.end());
+       if(s==t){
         return true;
+       }else{
+        return false;
+       }
     }
 };
