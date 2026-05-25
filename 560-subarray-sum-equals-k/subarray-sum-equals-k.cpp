307dff135d755e3fc3031e625491b5409603219c
ashu1osh9 <ashutosh.kr1818@gmail.com>
2026-05-25 18:48:03 +0530

    Time: 2494 ms (9.59%) | Memory: 34.4 MB (88.63%) - LeetSync

---
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -0,0 +1,54 @@
+// class Solution {
+// public:
+//     int subarraySum(vector<int>& nums, int k) {
+//         // with two pointer method
+//         int n=nums.size();
+//         int i=0;
+//         int j=0;
+//         int res=0;
+//         int sum=0;
+//         while(j<n){
+//              sum+=nums[j];
+//             // if(j==n-1){
+//             //     j=i;
+//             //     i++;
+//             //     sum=0;
+//             // }
+         
+//           if(sum>k){
+//             // sum-=nums[i];
+//             sum=0;
+//            j=i;
+//             i++;
+            
+//           }
+//           else if(sum==k){
+//             res++;
+//           }
+//           j++;
+//         }
+//         return res;
+//     }
+// };
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        // with two pointer method
+        int n=nums.size();
+        int i=0;
+        int j=0;
+        int res=0;
+        int sum=0;
+       for(int i=0;i<n;i++){
+        int sum=0;
+        for(int j=i;j<n;j++){
+             sum+=nums[j];
+             if(sum==k){
+               res++;
+             }
+        }
+       }
+        return res;
+    }
+};
