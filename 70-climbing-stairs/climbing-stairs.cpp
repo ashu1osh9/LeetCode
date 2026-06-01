0c6145374697358d7ba209af21ab87a81e5a3b0b
ashu1osh9 <ashutosh.kr1818@gmail.com>
2026-06-01 23:04:20 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (86.29%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        
+        // Base cases
+        if(n == 1) return 1;
+        if(n == 2) return 2;
+
+        int prev2 = 1; // ways to reach step 1
+        int prev1 = 2; // ways to reach step 2
+
+        for(int i = 3; i <= n; i++) {
+            int curr = prev1 + prev2;
+
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
+    }
+};
