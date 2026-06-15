0cc54f9bdcf687e5320a823539ee9a55cd80a65b
ashu1osh9 <ashutosh.kr1818@gmail.com>
2026-06-15 18:45:10 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (17.67%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
+    // memoniztion
+    vector<int>dp;
+    
+
+    int solve(int n) {
+    if(n == 0 || n == 1) return 1;
+    if(n == 2) return 2;
+    if(dp[n]!=-1){
+        return dp[n];
+    }
+    
+
+    int ways1 = solve(n - 1);
+    int ways2 = solve(n - 2);
+
+    return dp[n]=ways1 + ways2;
+}
     int climbStairs(int n) {
+        dp.assign(n+1,-1);
+        return solve(n);
         
-        // Base cases
-        if(n == 1) return 1;
-        if(n == 2) return 2;
-
-        int prev2 = 1; // ways to reach step 1
-        int prev1 = 2; // ways to reach step 2
-
-        for(int i = 3; i <= n; i++) {
-            int curr = prev1 + prev2;
-
-            prev2 = prev1;
-            prev1 = curr;
-        }
-
-        return prev1;
     }
 };
