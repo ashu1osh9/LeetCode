98c4306d4f85d975d1c5d92ed6d13a60b037e1f6
ashu1osh9 <ashutosh.kr1818@gmail.com>
2026-06-16 17:50:38 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (5.42%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,25 +1,34 @@
 class Solution {
 public:
-    int rob(vector<int>& nums) {
-
-        int n = nums.size();
-
-        if(n == 1) return nums[0];
-
-        int prev2 = nums[0];
-        int prev1 = max(nums[0], nums[1]);
-
-        for(int i = 2; i < n; i++) {
-
-            int take = nums[i] + prev2;
-            int skip = prev1;
-
-            int curr = max(take, skip);
-
-            prev2 = prev1;
-            prev1 = curr;
+    vector<int>dp;
+    int solve(vector<int>&nums,int n){
+        if(n==0){
+            return nums[0];
         }
+        if(n<0){
+            return 0 ;
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        int stolen = nums[n] + solve(nums,n-2);
+        int notstolen = solve(nums,n-1);
+        return dp[n] = max(stolen,notstolen);
+    }
 
-        return prev1;
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n==1){
+            return nums[0];
+
+        }
+        if(n==2){
+            return max(nums[0],nums[1]);
+
+        }
+        dp.assign(n,-1);
+        return solve(nums,n-1);
+
+        
     }
 };
