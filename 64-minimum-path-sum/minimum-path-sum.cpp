e1caf5be1c200307a934f0a5f43b61c6385bce85
ashu1osh9 <ashutosh.kr1818@gmail.com>
2026-06-20 02:31:40 +0530

    Time: 3 ms (29.23%) | Memory: 16.3 MB (59.77%) - LeetSync

---
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+ // recursion  
+//  int solve(vector<vector<int>>& grid,int m,int n){
+//     if(m==0 && n == 0){
+//         return grid[m][n];
+//     }
+//     if(m<0 || n<0){
+//         return 1e9; // not return 0; kyuki bahar wale path ko bhi recursion maan lega.
+//     }
+//     int up = grid[m][n] + solve(grid,m-1,n);
+//     int left = grid[m][n] + solve(grid,m,n-1);
+//     return min(up,left);
+//  }
+//     int minPathSum(vector<vector<int>>& grid) {
+//         int m = grid.size();
+//         int n = grid[0].size();
+//         return solve(grid,m-1,n-1);
+        
+//     }
+
+    vector<vector<int>> dp;
+
+    int solve(vector<vector<int>>& grid, int m, int n) {
+        if (m == 0 && n == 0) {
+            return grid[0][0];
+        }
+
+        if (m < 0 || n < 0) {
+            return 1e9;
+        }
+
+        if (dp[m][n] != -1) {
+            return dp[m][n];
+        }
+
+        int up = grid[m][n] + solve(grid, m - 1, n);
+        int left = grid[m][n] + solve(grid, m, n - 1);
+
+        return dp[m][n] = min(up, left);
+    }
+
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        dp.assign(m, vector<int>(n, -1));
+
+        return solve(grid, m - 1, n - 1);
+    }
+};
