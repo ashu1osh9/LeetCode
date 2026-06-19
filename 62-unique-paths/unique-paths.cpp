84ee24d41b7f7edbe630a5d9bdc5d4f17ca0c0bd
ashu1osh9 <ashutosh.kr1818@gmail.com>
2026-06-20 01:55:53 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (52.43%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,25 +1,59 @@
 class Solution {
 public: 
-// memonization
-vector<vector<int>>dp;
- int solve(int m , int n){
-    if(m==0 && n == 0){
-        return 1;
+// vector<vector<int>>dp;
+// // memoization
+//  int solve(int m , int n){
+//     if(m==0 && n == 0){
+//         return 1;
 
-    }
-    if(m<0 || n<0){
-        return 0;
-    }
-    if(dp[m][n]!=-1){
-        return dp[m][n];
-    }
-    int up = solve(m-1,n);
-    int right = solve(m,n-1);
-    return dp[m][n]= up + right;
+//     }
+//     if(m<0 || n<0){
+//         return 0;
+//     }
+//     if(dp[m][n]!=-1){
+//         return dp[m][n];
+//     }
+//     int up = solve(m-1,n);
+//     int right = solve(m,n-1);
+//     return dp[m][n]= up + right;
     
- }
+//  }
+
+// dp top down 
+
+    vector<vector<int>> dp;
+
+    int solve(int m, int n) {
+
+        dp[0][0] = 1;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (i == 0 && j == 0) {
+                    continue;
+                }
+
+                int up = 0;
+                int left = 0;
+
+                if (i > 0) {
+                    up = dp[i - 1][j];
+                }
+
+                if (j > 0) {
+                    left = dp[i][j - 1];
+                }
+
+                dp[i][j] = up + left;
+            }
+        }
+
+        return dp[m - 1][n - 1];
+    }
+
     int uniquePaths(int m, int n) {
-        dp.resize(m,vector<int>(n,-1));
-        return solve(m-1,n-1);
+        dp.assign(m, vector<int>(n, 0));
+        return solve(m, n);
     }
 };
