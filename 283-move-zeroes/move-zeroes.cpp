46f534c802dfb9709e5b41825934b9a6efc4c101
ashu1osh9 <ashutosh.kr1818@gmail.com>
2026-07-20 23:12:07 +0530

    Time: 0 ms (100.00%) | Memory: 25 MB (5.04%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,17 +1,36 @@
 
+// class Solution {
+// public:
+//     void moveZeroes(vector<int>& nums) {
+
+//         int j = 0;
+
+//         for (int i = 0; i < nums.size(); i++) {
+
+//             if (nums[i] != 0) {
+//                 swap(nums[i], nums[j]);
+//                 j++;
+//             }
+
+//         }
+//     }
+// };
+
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
 
-        int j = 0;
+        vector<int> temp;
 
         for (int i = 0; i < nums.size(); i++) {
-
-            if (nums[i] != 0) {
-                swap(nums[i], nums[j]);
-                j++;
-            }
-
+            if (nums[i] != 0)
+                temp.push_back(nums[i]);
         }
+
+        while (temp.size() < nums.size()) {
+            temp.push_back(0);
+        }
+
+        nums = temp;
     }
 };
