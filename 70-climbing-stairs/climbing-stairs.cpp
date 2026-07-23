7eec5ac044a21fe2b00ea4439246a98b4fffa65e
ashu1osh9 <ashutosh.kr1818@gmail.com>
2026-07-23 14:41:37 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (20.29%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,49 +1,104 @@
 // class Solution {
 // public:
-//     // memoniztion
-//     vector<int>dp;
+//     int solve(int n ){
+//         if(n==1){
+//             return 1;
 
-
-//     int solve(int n) {
-//     if(n == 0 || n == 1) return 1;
-//     if(n == 2) return 2;
-//     if(dp[n]!=-1){
-//         return dp[n];
-//     }
+//         }
+//         if(n==2){
+//             return 2;
+//         }
+//      int step1 = solve(n-1);
+//      int step2 = solve(n-2);
     
+//      return step1 + step2;
 
-//     int ways1 = solve(n - 1);
-//     int ways2 = solve(n - 2);
 
-//     return dp[n]=ways1 + ways2;
-// }
+//     }
 //     int climbStairs(int n) {
-//         dp.assign(n+1,-1);
 //         return solve(n);
         
 //     }
-
- // top down approach 
+// };
 
 class Solution {
 public:
-    vector<int> dp;
+vector<int>dp;
 
-    int topdown(int n) {
+    int solve(int n ){
+        if(n==1){
+            return 1;
 
-        dp[0] = 1;
-        if(n >= 1) dp[1] = 1;
-        if(n >= 2) dp[2] = 2;
-
-        for(int i = 3; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];  
         }
+        if(n==2){
+            return 2;
+        }
+        if(dp[n]!= -1 ){
+            return dp[n];
+        }
+     int step1 = solve(n-1);
+     int step2 = solve(n-2);
+    
+     return dp[n] = step1 + step2;
 
-        return dp[n];
+
     }
-
     int climbStairs(int n) {
-        dp.assign(n + 1, 0);   
-        return topdown(n);
+        dp.assign(n+1,-1);
+        return solve(n);
+        
     }
 };
+
+
+
+
+// // class Solution {
+// // public:
+// //     // memoniztion
+// //     vector<int>dp;
+
+
+// //     int solve(int n) {
+// //     if(n == 0 || n == 1) return 1;
+// //     if(n == 2) return 2;
+// //     if(dp[n]!=-1){
+// //         return dp[n];
+// //     }
+    
+
+// //     int ways1 = solve(n - 1);
+// //     int ways2 = solve(n - 2);
+
+// //     return dp[n]=ways1 + ways2;
+// // }
+// //     int climbStairs(int n) {
+// //         dp.assign(n+1,-1);
+// //         return solve(n);
+        
+// //     }
+
+//  // top down approach 
+
+// class Solution {
+// public:
+//     vector<int> dp;
+
+//     int topdown(int n) {
+
+//         dp[0] = 1;
+//         if(n >= 1) dp[1] = 1;
+//         if(n >= 2) dp[2] = 2;
+
+//         for(int i = 3; i <= n; i++) {
+//             dp[i] = dp[i - 1] + dp[i - 2];  
+//         }
+
+//         return dp[n];
+//     }
+
+//     int climbStairs(int n) {
+//         dp.assign(n + 1, 0);   
+//         return topdown(n);
+//     }
+// };
