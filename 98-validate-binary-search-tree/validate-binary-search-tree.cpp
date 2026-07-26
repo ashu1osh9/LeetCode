055a5381dd413f2d5a23a01f5284ff0edd7197b7
ashu1osh9 <ashutosh.kr1818@gmail.com>
2026-07-26 17:37:22 +0530

    Time: 2 ms (6.33%) | Memory: 22.1 MB (16.32%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -9,30 +9,62 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
- // recursive dfs 
+
+//   // recursive dfs  == T.C => O(N) , S.C => 0(N)
+// class Solution {
+// public:
+//     long long  curr = LLONG_MIN;
+//     int ans = 1;
+
+//     void dfs(TreeNode* root){
+//         if(root == NULL || ans == 0 ){
+//             return ;
+//         }
+
+//         dfs(root->left);
+//         if(root->val<=curr){
+//             ans = 0;
+//             return ;
+//         }
+//         curr = root ->val;
+//         dfs(root->right);
+        
+//     }
+//     bool isValidBST(TreeNode* root) {
+//         dfs(root);
+//         if(ans == 0){
+//             return false;
+//         }
+//         return true;
+        
+//     }
+// };
+// stack 
 class Solution {
 public:
-    long long  curr = LLONG_MIN;
-    int ans = 1;
-
-    void dfs(TreeNode* root){
-        if(root == NULL || ans == 0 ){
-            return ;
-        }
-
-        dfs(root->left);
-        if(root->val<=curr){
-            ans = 0;
-            return ;
-        }
-        curr = root ->val;
-        dfs(root->right);
-        
-    }
+ long long ans = LLONG_MIN;
     bool isValidBST(TreeNode* root) {
-        dfs(root);
-        if(ans == 0){
-            return false;
+        stack<TreeNode*>st;
+        TreeNode* curr = root;
+        while(curr != NULL || !st.empty()){
+
+
+            while(curr){
+                st.push(curr);
+                curr = curr->left;
+            }
+
+            TreeNode * data = st.top();
+            st.pop();
+            if(data->val<=ans){
+                
+                return false;
+            }
+            ans = data->val;
+
+            curr = data ->right;
+
+
         }
         return true;
         
