c02116cdca6c586e151b16fd3ab2da9f98766a04
ashu1osh9 <ashutosh.kr1818@gmail.com>
2026-07-26 17:10:55 +0530

    Time: 0 ms (100.00%) | Memory: 24.3 MB (66.89%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
@@ -12,37 +12,53 @@
 
 
 
- // T.c => O(v) S.C => O(V) 
-class Solution {
-public:
-vector<int>inorder;
-
-    void dfs(TreeNode* root ){
-        if(root == NULL){
-            return ;
-        }
-        dfs(root->left);
-        inorder.push_back(root->val);
-        dfs(root->right);
-    }
-    int kthSmallest(TreeNode* root, int k) {
-        inorder.clear();
-        dfs(root);
-
-        return inorder[k-1];
-    }
-};
+//  // T.c => O(v) S.C => O(V) 
 // class Solution {
 // public:
-//     int kthSmallest(TreeNode* root, int k) {
+// vector<int>inorder;
+
+//     void dfs(TreeNode* root ){
 //         if(root == NULL){
-//             return -1;
+//             return ;
 //         }
-//         while(k){
-//             root = root->left;
-//             k--;
-//         }
-//         return root->val;
-        
+//         dfs(root->left);
+//         inorder.push_back(root->val);
+//         dfs(root->right);
+//     }
+//     int kthSmallest(TreeNode* root, int k) {
+//         inorder.clear();
+//         dfs(root);
+
+//         return inorder[k-1];
 //     }
 // };
+class Solution {
+public:
+
+    int count = 0;
+    int ans = 0;
+
+    void dfs(TreeNode* root, int k){
+
+        if(root == NULL)
+            return;
+
+        dfs(root->left, k);
+
+        count++;
+
+        if(count == k){
+            ans = root->val;
+            return;
+        }
+
+        dfs(root->right, k);
+    }
+
+    int kthSmallest(TreeNode* root, int k) {
+
+        dfs(root, k);
+
+        return ans;
+    }
+};
