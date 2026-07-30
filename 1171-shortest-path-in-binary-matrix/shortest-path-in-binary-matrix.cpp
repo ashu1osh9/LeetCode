c8611644d04d95e335f1fbcbdfa5dcf1fd28307a
ashu1osh9 <ashutosh.kr1818@gmail.com>
2026-07-30 18:13:47 +0530

    Time: 7 ms (97.00%) | Memory: 25.1 MB (63.51%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -1,57 +1,113 @@
+// // T.C => O(N)
+// class Solution {
+// public:
+//     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+
+//         int n = grid.size();
+
+//         // Agar start ya end blocked hai
+//         if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1)
+//             return -1;
+
+//         vector<vector<bool>> visited(n, vector<bool>(n, false));
+
+//         vector<pair<int, int>> dir = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1},
+//                                       {0, 1},   {1, -1}, {1, 0},  {1, 1}};
+
+//         queue<pair<int, int>> q;
+
+//         q.push({0, 0});
+//         visited[0][0] = true;
+
+//         int count = 1;
+
+//         while (!q.empty()) {
+
+//             int levelSize = q.size();
+
+//             while (levelSize--) {
+
+//                 auto front = q.front();
+//                 q.pop();
+
+//                 int x = front.first;
+//                 int y = front.second;
+
+//                 // Destination mil gaya
+//                 if (x == n - 1 && y == n - 1)
+//                     return count;
+
+//                 // 8 directions me move karo
+//                 for (auto d : dir) {
+
+//                     int nx = x + d.first;
+//                     int ny = y + d.second;
+
+//                     if (nx >= 0 && ny >= 0 && nx < n && ny < n &&
+//                         grid[nx][ny] == 0 && !visited[nx][ny]) {
+
+//                         visited[nx][ny] = true;
+//                         q.push({nx, ny});
+//                     }
+//                 }
+//             }
+
+//             count++;
+//         }
+
+//         return -1;
+//     }
+// };
+
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
 
         int n = grid.size();
 
-        // Agar start ya end blocked hai
-        if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1)
+        // Start ya End blocked hai
+        if (grid[0][0] == 1 || grid[n-1][n-1] == 1)
             return -1;
 
         vector<vector<bool>> visited(n, vector<bool>(n, false));
 
-        vector<pair<int, int>> dir = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1},
-                                      {0, 1},   {1, -1}, {1, 0},  {1, 1}};
+        vector<pair<int,int>> dir = {
+            {-1,-1}, {-1,0}, {-1,1},
+            { 0,-1},          { 0,1},
+            { 1,-1}, { 1,0},  { 1,1}
+        };
 
-        queue<pair<int, int>> q;
+        queue<pair<pair<int,int>, int>> q;
 
-        q.push({0, 0});
+        q.push({{0,0}, 1});
         visited[0][0] = true;
 
-        int count = 1;
+        while(!q.empty()) {
 
-        while (!q.empty()) {
+            auto front = q.front();
+            q.pop();
 
-            int levelSize = q.size();
+            int x = front.first.first;
+            int y = front.first.second;
+            int dist = front.second;
 
-            while (levelSize--) {
+            // Destination mil gaya
+            if(x == n-1 && y == n-1)
+                return dist;
 
-                auto front = q.front();
-                q.pop();
+            for(auto d : dir) {
 
-                int x = front.first;
-                int y = front.second;
+                int nx = x + d.first;
+                int ny = y + d.second;
 
-                // Destination mil gaya
-                if (x == n - 1 && y == n - 1)
-                    return count;
+                if(nx >= 0 && ny >= 0 && nx < n && ny < n &&
+                   grid[nx][ny] == 0 &&
+                   !visited[nx][ny]) {
 
-                // 8 directions me move karo
-                for (auto d : dir) {
-
-                    int nx = x + d.first;
-                    int ny = y + d.second;
-
-                    if (nx >= 0 && ny >= 0 && nx < n && ny < n &&
-                        grid[nx][ny] == 0 && !visited[nx][ny]) {
-
-                        visited[nx][ny] = true;
-                        q.push({nx, ny});
-                    }
+                    visited[nx][ny] = true;
+                    q.push({{nx, ny}, dist + 1});
                 }
             }
-
-            count++;
         }
 
         return -1;
