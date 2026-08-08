67c5b9b9f8f7287076a812299841f5f5c4cd9531
ashu1osh9 <ashutosh.kr1818@gmail.com>
2026-08-08 14:52:02 +0530

    Time: 3 ms (0.54%) | Memory: 13 MB (13.66%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -9,37 +9,28 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+ // dfs -- inorder/ preorder.postorder se kar sakte hai -- 
+ // T.C => O(N) , S.C => O(N) -- 
 class Solution {
 public:
-    bool isSameTree(TreeNode* p, TreeNode* q) {
 
-        queue<TreeNode*>q1,q2;
-        
-        q1.push(p);
-        q2.push(q);
-        while(!q1.empty() || !q2.empty()){
-            TreeNode* curr1 =  q1.front();
-            TreeNode* curr2 = q2.front();
-            q1.pop();
-            q2.pop();
-            if(curr1 == NULL && curr2 == NULL){
-                continue;
-            }
-            if(curr1 == NULL || curr2 == NULL){
-                return false;
-            }
-            if(curr1->val != curr2->val){
-                return false;
+    vector<int>ans1, ans2;
 
-            }
-            q1.push(curr1->left);
-            q1.push(curr1->right);
-            q2.push(curr2->left);
-            q2.push(curr2->right);
-
+    void preorder(TreeNode* root,vector<int>&ans){
+        if(root == NULL){
+           ans.push_back(INT_MIN);
+            return ;
         }
 
-        return q1.empty() && q2.empty();
+        ans.push_back(root->val);
+        preorder(root->left,ans);
+        preorder(root->right,ans);
+    }
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        preorder(p,ans1);
+        preorder(q,ans2);
+
+        return ans1 == ans2;
         
     }
 };
