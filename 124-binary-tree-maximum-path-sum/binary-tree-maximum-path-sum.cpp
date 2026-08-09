d1942148378b37d35a9ea7331c46a6e2bd1c7c67
ashu1osh9 <ashutosh.kr1818@gmail.com>
2026-08-09 08:38:42 +0530

    Time: 5 ms (3.75%) | Memory: 29.2 MB (9.33%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
@@ -9,60 +9,30 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-//  // T.C => O(N) , S.C => O(H) --
-// class Solution {
-// public:
-//     int path = INT_MIN;
-//     int solve(TreeNode* root){
-//         if(root == NULL){
-//             return 0;
-//         }
-
-//         int left = root->val + max(0,solve(root->left));
-//         int right = root->val + max(0,solve(root->right));
-
-//         path = max(path , left + right - root->val);
-
-//         return max(left,right);
-//     }
-//     int maxPathSum(TreeNode* root) {
-
-//         solve(root);
-//         return path;
-        
-//     }
-// };
-
-
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    int maxsum;
+    int maxSum = INT_MIN;
     int solve(TreeNode* root){
-        if(root==NULL)return 0;
-        int l=solve(root->left);
-        int r=solve(root->right);
-        int bothpaths=l+r+root->val;
-        int bestpath=max(l,r)+root->val;
-        int onlyroot=root->val;
-        maxsum=max({maxsum,bothpaths,bestpath,onlyroot});
-        return max(bestpath,onlyroot);
+        if(root == NULL){
+            return 0;
+        }
+
+        int l = solve(root->left);
+        int r = solve(root->right);
+        
+        int neeche_hi_mil_gaya = l + r + root->val;
+        int koi_ek_acche_hai = max(l,r) + root->val;
+        int ek_root_accha_hai = root->val;
+        maxSum = max({maxSum,neeche_hi_mil_gaya,koi_ek_acche_hai,ek_root_accha_hai});
+
+        return max(koi_ek_acche_hai,ek_root_accha_hai);
 
     }
+
+
     int maxPathSum(TreeNode* root) {
-        maxsum=INT_MIN;
         solve(root);
-        return maxsum;
+        return maxSum;
         
     }
 };
