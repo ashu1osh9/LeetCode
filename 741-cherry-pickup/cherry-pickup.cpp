ca018df1529c42bdb1b8c2fb5c33db2d1b69f019
ashu1osh9 <ashutosh.kr1818@gmail.com>
2026-08-14 12:46:46 +0530

    Time: 673 ms (5.09%) | Memory: 566.8 MB (5.12%) - LeetSync

--- 741-cherry-pickup/cherry-pickup.cpp
+++ 741-cherry-pickup/cherry-pickup.cpp
@@ -51,7 +51,7 @@
 //     }
 // };
 
-// memoization -- 
+// memoization -- with dp size 4 
 class Solution {
 public:
 
@@ -133,3 +133,105 @@ public:
         return max(0, solve(grid, 0, 0, 0, 0));
     }
 };
+
+// memiozation with dp size 3 -- optimized approach 
+
+// i1 + j1 = steps 
+// i2 + j2 = steps --
+// j1 = i2 + j2 -i1
+// class Solution {
+// public:
+
+//     vector<vector<vector<int>>> dp;
+
+//     int solve(vector<vector<int>>& grid, int i1, int j1, int i2) {
+
+//         int n = grid.size();
+
+//         // j2 calculate karo
+//         int j2 = i1 + j1 - i2;
+
+//         // Out of bound
+//         if (i1 >= n || j1 >= n || i2 >= n || j2 >= n)
+//             return -1e9;
+
+//         // Thorn / blocked cell
+//         if (grid[i1][j1] == -1 || grid[i2][j2] == -1)
+//             return -1e9;
+
+//         // Already calculated
+//         if (dp[i1][j1][i2] != -1)
+//             return dp[i1][j1][i2];
+
+//         // Dono end par pahunch gaye
+//         if (i1 == n - 1 && j1 == n - 1 &&
+//             i2 == n - 1 && j2 == n - 1) {
+
+//             return dp[i1][j1][i2] = grid[i1][j1];
+//         }
+
+//         // Current cherries
+//         int cherries = 0;
+
+//         if (i1 == i2 && j1 == j2) {
+
+//             if (grid[i1][j1] == 1)
+//                 cherries++;
+
+//         } else {
+
+//             if (grid[i1][j1] == 1)
+//                 cherries++;
+
+//             if (grid[i2][j2] == 1)
+//                 cherries++;
+//         }
+
+//         // 4 choices
+
+//         // Person 1 -> Right
+//         // Person 2 -> Right
+//         int right_right =
+//             solve(grid, i1, j1 + 1, i2);
+
+//         // Person 1 -> Right
+//         // Person 2 -> Down
+//         int right_down =
+//             solve(grid, i1, j1 + 1, i2 + 1);
+
+//         // Person 1 -> Down
+//         // Person 2 -> Right
+//         int down_right =
+//             solve(grid, i1 + 1, j1, i2);
+
+//         // Person 1 -> Down
+//         // Person 2 -> Down
+//         int down_down =
+//             solve(grid, i1 + 1, j1, i2 + 1);
+
+//         int best = max({
+//             right_right,
+//             right_down,
+//             down_right,
+//             down_down
+//         });
+
+//         return dp[i1][j1][i2] = cherries + best;
+//     }
+
+
+//     int cherryPickup(vector<vector<int>>& grid) {
+
+//         int n = grid.size();
+
+//         // 3D DP
+//         dp.resize(n,
+//             vector<vector<int>>(
+//                 n,
+//                 vector<int>(n, -1)
+//             )
+//         );
+
+//         return max(0, solve(grid, 0, 0, 0));
+//     }
+// };
