b8813f1165064a015f0badb9408ff1efa6bdade6
ashu1osh9 <ashutosh.kr1818@gmail.com>
2026-09-09 16:59:19 +0530

    Time: 75 ms (42.72%) | Memory: 14 MB (27.97%) - LeetSync

---
+++ 279-perfect-squares/perfect-squares.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+vector<int>dp;
+    int solve(int n){
+
+        if(n==0) return 0;
+
+        if(dp[n] != -1) {
+            return dp[n];
+            }
+        int ans = INT_MAX;
+
+        for(int i = 1;i*i<=n;i++){
+
+            int sq = i*i;
+            int rem = n - sq;
+
+            int curr = 1 + solve(rem);
+
+            ans = min(ans,curr);
+
+        }
+        return dp[n] = ans;
+    }
+    int numSquares(int n) {
+        dp.assign(n+1,-1);
+
+        return solve(n);
+        
+    }
+};
