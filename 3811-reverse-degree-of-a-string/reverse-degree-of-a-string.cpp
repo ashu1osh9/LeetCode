615a3fe4f481e301497451e4b4746c2dbaf122ca
ashu1osh9 <ashutosh.kr1818@gmail.com>
2026-09-20 13:19:48 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (47.88%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -1,23 +1,13 @@
 class Solution {
 public:
     int reverseDegree(string s) {
-        unordered_map<char,int>map;
-        int cnt = 26;
+        int sum = 0;
 
-        for(char ch = 'a'; ch <= 'z'; ch++) {
-
-
-            map[ch] = cnt;
-            cnt--;
-    
-}
-int sum = 0;
-
-          for(int i = 0; i < s.size(); i++) {
-            int val = map[s[i]];
+        for(int i = 0; i < s.size(); i++) {
+            int val = 'z' - s[i] + 1;
             sum += val * (i + 1);
         }
+
         return sum;
-        
     }
 };
